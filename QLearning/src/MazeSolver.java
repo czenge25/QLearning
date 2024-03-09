04274f7b259e8b826805310948f05459d44c344b
czenge25 <czenge25@kentdenver.org>
2024-03-09 12:34:23 -0500

    Instance data of MazeSolver

---
+++ QLearning/src/MazeSolver.java
@@ -0,0 +1,13 @@
+public class MazeSolver {
+
+    private final double A = 0.1;
+    private final double G = 0.9;
+
+    private final int reward = 100;
+    private final int penalty = -10;
+
+    private int[][] R;
+    private double[][] Q;
+
+
+}
