c6433b0915b543fbada1b8a05699e4804f56a5bb
czenge25 <czenge25@kentdenver.org>
2024-03-13 10:56:49 -0600

    New run method

--- QLearning/src/Main.java
+++ QLearning/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MazeSolver solver = new MazeSolver();
+        solver.run();
+        solver.printQ();
+
     }
 }
